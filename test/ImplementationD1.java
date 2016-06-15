264e5db295a04f9fc36b4626130fd0f14b2bd28e
Roger <Jaume@172.25.3.237>
2016-06-15 15:28:47 +0200

    afegit ImplementationD1

---
+++ test/ImplementationD1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jaume
+ */
+public class ImplementationD1 implements InterfaceD {
+    private int i;
+    public ImplementationD1(int i){
+        this.i = i;
+    }
+}
