a724eaa95a9089281e6deec338517c880bdc3adc
“Daniel <danigv19@gmail.com>
2016-06-15 18:39:51 +0200

    Inici testos

--- test/Testos/Test1.java
+++ test/Testos/Test1.java
@@ -4,11 +4,43 @@
  * and open the template in the editor.
  */
 package Testos;
-
+import Factories2.*;
+import Implementations1.*;
+import java.util.HashMap;
+import java.util.Map;
+import servicelocator.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
 /**
  *
  * @author Roger & Daniel
  */
 public class Test1 {
+    
+    private ServiceLocator ssl;
+    private ServiceLocator csl;
+    private Factory fA1;
+    private Factory fB1;
+    private Factory fC1;
+    private Factory fD1;
+    private Map<String,Factory> map;
+    private Object o1;
+    private Object o2;
+    
+    
+    
+    @Before
+    public void Test1(){
+        
+        map = new HashMap<>();
+        
+        
+        ssl = new SimpleServiceLocator(map);
+        csl = new CachedServiceLocator(map);
+  
+        
+        
+    }
+    
     
 }
