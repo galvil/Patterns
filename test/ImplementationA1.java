ee1fe699c8dd50b1e1f045bb7ce5d6b2e52ed916
“Daniel <danigv19@gmail.com>
2016-06-15 15:28:06 +0200

    Implementacions Interficies

---
+++ test/ImplementationA1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author galvil
+ */
+public class ImplementationA1 implements InterfaceA{
+    private InterfaceB b;
+    private InterfaceC c;
+    public ImplementationA1(InterfaceB b, InterfaceC c){
+        this.b = b; this.c = c;
+    }
+}
