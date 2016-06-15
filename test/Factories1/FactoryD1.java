f9dbe0ecf637b25bea58600a9b96fab9c83725c5
Roger <Jaume@172.25.3.237>
2016-06-15 18:37:29 +0200

    FactoryC1 i FactoryD1

--- test/Factories1/FactoryD1.java
+++ test/Factories1/FactoryD1.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package Factories1;
-
+import Interfaces.*;
+import servicelocator.*;
+import Implementations1.*;
 /**
  *
  * @author Roger & Daniel
  */
-public class FactoryD1 {
+public class FactoryD1 implements Factory{
+
+    @Override
+    public Object create(ServiceLocator s) throws LocatorError {
+        try{
+            int i = (int) s.getObject("D");
+            return new ImplementationD1(i);
+        }catch(ClassCastException ex){
+            throw new LocatorError(ex);
+        }
+    }
     
 }
