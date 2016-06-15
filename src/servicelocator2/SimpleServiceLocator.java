33803e57e0b8ea518ddf53ac67f9e515b15808f8
“Daniel <danigv19@gmail.com>
2016-06-15 17:02:38 +0200

    sets fets

--- src/servicelocator2/SimpleServiceLocator.java
+++ src/servicelocator2/SimpleServiceLocator.java
@@ -5,20 +5,40 @@
  */
 package servicelocator2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Roger & Daniel
  */
 public class SimpleServiceLocator implements ServiceLocator {
+    
+    private Map<Class,Factory> mapaFactories;
+    private Map<Class,Object> mapaConstants;
 
+     public SimpleServiceLocator(){
+        mapaFactories = new HashMap<>();
+        mapaConstants = new HashMap<>();
+    }
+    
+    
     @Override
     public <T> void setService(Class<T> klass, Factory<T> factory) throws LocatorError {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        if(mapaFactories.containsKey(klass)) throw new LocatorError();
+        if (mapaConstants.containsKey(klass)) throw new LocatorError();
+        else mapaFactories.put(klass, factory);
+            
     }
 
     @Override
     public <T> void setConstant(Class<T> klass, T value) throws LocatorError {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if(mapaFactories.containsKey(klass)) throw new LocatorError();
+        if (mapaConstants.containsKey(klass)) throw new LocatorError();
+        else mapaConstants.put(klass,value);
+    
     }
 
     @Override
